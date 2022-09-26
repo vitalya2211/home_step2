6a1c9d22f9bc82c570bdf16302f96a52277c99ed
Vitalya <vitalya.2211@yandex.ru>
2022-09-26 22:01:44 +1000

    finish task2

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,82 @@
 #include<iostream>
 using namespace std;
+void print_happy()
+{
+	cout << "Ваш билет счастливый, может стоит его сьесть?.." << endl;
+}
+void print_unhappy()
+{
+	cout << "следующий обязательно будет счастливым)" << endl;
+}
+int double_num(int *num)
+{
+	int result = *num % 1000;
+	int num1 = 0, num2 = 0;
+	*num /= 1000;
+	while (*num)
+	{
+		num1 += *num % 10;
+		num2 += result % 10;
+		*num /= 10;
+		result /= 10;
+	}
+	*num = num1;
+	return num2;
+
+}
+void summ(int n)
+{
+	int num=double_num(&n);
+	
+	if (num == n)
+		print_happy();
+	else
+		print_unhappy();
+}
+void sqert_bus(int n)
+{
+	int result = n % 1000;
+	n /= 1000;
+	int num1 = 0, num2 = 0;
+	while (n )
+	{
+		num1 += n % 10;
+		num2 += result % 10;
+		n /= 10;
+		result /= 10;
+	}
+	while (true)
+	{
+		if ((num1 < 10) & (num2 < 10))
+			break;
+		else
+		{
+			while (num1 > 10)
+			{
+				num1 = (num1 / 10) + (num1 % 10);
+			}
+			while (num2 > 10)
+			{
+				num2 = (num2 / 10) + (num2 % 10);
+			}
+		}
+	}
+	if (num1 == num2)
+		print_happy();
+	else
+		print_unhappy();
+}
+void div(int n)
+{
+	int result = n % 1000;
+	n /= 1000;
+	if ((result % 9) == (n % 9))
+		print_happy();
+	else
+		print_unhappy();
+	cout << n % 9 << endl << result % 9 << endl;
+
+}
 bool polindrom(int n) 
 {
 	//bool result;
@@ -18,7 +95,7 @@ bool polindrom(int n)
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int poli;
+	int poli,bus_tiket,select_happy;
 	//1. Пользователь вводит число.Программа говорит, является ли введенное число палиндромом.
 	//Палиндром - это число, которое читается одинаково, как слева направо, так и справа налево.
 	
@@ -39,7 +116,39 @@ int main()
 Также существует определение «счастливости», согласно которому совпадать должны не сами суммы, 
 а их числовые корни (или, эквивалентно, остатки при делении на 9) — в таком случае счастливых 
 билетов больше.*/
-
+	cout << "программа определения счастливого билета\nВведите 6-ти значное число ->";
+	cin >> bus_tiket;
+	if (!bus_tiket)
+	{
+		cout << "билет с номером 0, считаем счатсливым)" << endl;
+		system("pause");
+		return 0;
+	}
+	else if (bus_tiket > 999999 || bus_tiket < 100000)
+	{
+		cout << "Введен не верный номер" << endl;
+		system("pause");
+		return 0;
+	}
+	cout << "Выберите каким способом определять счастье ->\n\
+1.Сумма первых 3х совпадает с суммой последних.\n\
+2.Совпадение корнями.\n\
+3.остатки при делении на 9\n";
+	cin >> select_happy;
+	switch (select_happy)
+	{
+	case 1:
+		summ(bus_tiket);
+		break;
+	case 2:
+		sqert_bus(bus_tiket);
+		break;
+	case 3:
+		div(bus_tiket);
+	default:
+		break;
+	}
+	
 
 
 	system("pause");
